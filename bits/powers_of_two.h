ad62f2741574f741430812e7b64b8cfa0c620f87
Brian Brooks <brooks.brian@gmail.com>
2015-10-09 00:49:23 -0500

    Add floor2 & ceil2

---
+++ bits/powers_of_two.h
@@ -0,0 +1,25 @@
+#ifndef BITS_POWERS_OF_TWO_H
+#define BITS_POWERS_OF_TWO_H
+
+// Greatest power of 2 less than or equal to x.
+static inline unsigned int floor2(unsigned int x) {
+  x = x | (x >> 1);
+  x = x | (x >> 2);
+  x = x | (x >> 4);
+  x = x | (x >> 8);
+  x = x | (x >> 16);
+  return x - (x >> 1);
+}
+
+// Least power of 2 greater than or equal to x.
+static inline unsigned int ceil2(unsigned int x) {
+  x = x - 1;
+  x = x | (x >> 1);
+  x = x | (x >> 2);
+  x = x | (x >> 4);
+  x = x | (x >> 8);
+  x = x | (x >> 16);
+  return x + 1;
+}
+
+#endif  // BITS_POWERS_OF_TWO_H
---
+++ bits/powers_of_two_test.cc
@@ -0,0 +1,21 @@
+#include "gtest/gtest.h"
+
+#include "bits/powers_of_two.h"
+
+TEST(PowersOfTwoTest, floor2) {
+  EXPECT_EQ(0, floor2(0));
+  EXPECT_EQ(1, floor2(1));
+  EXPECT_NE(1, floor2(2));
+  EXPECT_EQ(2, floor2(2));
+
+  EXPECT_EQ(1<<4, floor2( (1<<4)+1 ));
+  EXPECT_NE(1<<4, floor2(  1<<5 ));
+}
+
+TEST(PowersOfTwoTest, ceil2) {
+  EXPECT_EQ(0, ceil2(0));
+  EXPECT_EQ(1, ceil2(1));
+  EXPECT_NE(1, ceil2(2));
+
+  EXPECT_EQ(1<<4, ceil2( (1<<4)-1 ));
+}
