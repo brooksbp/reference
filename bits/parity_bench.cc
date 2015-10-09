ede49d2dd27f122bd29892b38de440ab716cadbe
Brian Brooks <brooks.brian@gmail.com>
2015-10-09 01:14:36 -0500

    Add parity

---
+++ bits/parity_bench.cc
@@ -0,0 +1,31 @@
+#include "benchmark/benchmark.h"
+
+#include "bits/parity.h"
+
+static volatile int x = 0xF0F0F0F0;
+
+static void BM_parity_scan(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    benchmark::DoNotOptimize(parity_scan(x));
+  }
+  state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_parity_scan);
+
+static void BM_parity_mul(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    benchmark::DoNotOptimize(parity_mul(x));
+  }
+  state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_parity_mul);
+
+static void BM_builtin_parity(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    benchmark::DoNotOptimize(parity(x));
+  }
+  state.SetItemsProcessed(state.iterations());
+}
+BENCHMARK(BM_builtin_parity);
+
+BENCHMARK_MAIN();
---
+++ bits/parity_test.cc
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+#include "bits/parity.h"
+
+TEST(ParityTest, ResultsAreSame) {
+  EXPECT_EQ(parity(0xF0F0F0F0), parity_scan(0xF0F0F0F0));
+  EXPECT_EQ(parity(0xF0F0F0F0), parity_mul(0xF0F0F0F0));
+}
