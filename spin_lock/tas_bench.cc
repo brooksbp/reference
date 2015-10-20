5fe483a0f6f02e2c938440605887d7cd9021b39a
Brian Brooks <brooks.brian@gmail.com>
2015-10-19 21:10:52 -0500

    Add spin_lock/ticket

--- spin_lock/tas_bench.cc
+++ spin_lock/tas_bench.cc
@@ -1,18 +1,51 @@
 #include "benchmark/benchmark.h"
 
 #include "spin_lock/tas.h"
+#include "spin_lock/ticket.h"
+
+const int kNumCriticalSections = 10000000;
 
 static struct tas_lock f;
 
 static void BM_TAS(benchmark::State& state) {
   while (state.KeepRunning()) {
-    for (int i = 0; i < 100000000; ++i) {
+    for (int i = 0; i < kNumCriticalSections; ++i) {
       tas_lock_acquire(&f);
       tas_lock_release(&f);
     }
   }
-  state.SetItemsProcessed(state.iterations() * 100000000);
+  state.SetItemsProcessed(state.iterations() * kNumCriticalSections);
 }
 BENCHMARK(BM_TAS)->ThreadPerCpu();
+
+static struct ticket_lock t1;
+
+static void BM_TicketLock(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    for (int i = 0; i < kNumCriticalSections; ++i) {
+      ticket_lock_acquire(&t1);
+      ticket_lock_release(&t1);
+    }
+  }
+  state.SetItemsProcessed(state.iterations() * kNumCriticalSections);
+}
+BENCHMARK(BM_TicketLock)->ThreadPerCpu();
+
+static struct ticket_lock t2;
+
+static void BM_TicketLockBackoff(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    for (int i = 0; i < kNumCriticalSections; ++i) {
+      ticket_lock_acquire_bo(&t2, state.range_x());
+      ticket_lock_release(&t2);
+    }
+  }
+  state.SetItemsProcessed(state.iterations() * kNumCriticalSections);
+}
+BENCHMARK(BM_TicketLockBackoff)->Arg(1<<1)->ThreadPerCpu();
+BENCHMARK(BM_TicketLockBackoff)->Arg(1<<2)->ThreadPerCpu();
+BENCHMARK(BM_TicketLockBackoff)->Arg(1<<3)->ThreadPerCpu();
+BENCHMARK(BM_TicketLockBackoff)->Arg(1<<4)->ThreadPerCpu();
+BENCHMARK(BM_TicketLockBackoff)->Arg(1<<5)->ThreadPerCpu();
 
 BENCHMARK_MAIN();
---
+++ spin_lock/ticket.h
@@ -0,0 +1,41 @@
+#ifndef SPIN_LOCK_TICKET_H
+#define SPIN_LOCK_TICKET_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct ticket_lock {
+  int next_ticket;
+  int now_serving;
+};
+
+void ticket_lock_acquire(struct ticket_lock *lock) {
+  int my_ticket = __atomic_fetch_add(&lock->next_ticket, 1, __ATOMIC_RELAXED);
+  while (__atomic_load_n(&lock->now_serving, __ATOMIC_RELAXED) != my_ticket)
+    ;  // spin
+  __atomic_thread_fence(__ATOMIC_ACQUIRE);
+}
+
+void ticket_lock_release(struct ticket_lock *lock) {
+  int t = lock->now_serving + 1;
+  __atomic_store_n(&lock->now_serving, t, __ATOMIC_RELEASE);
+}
+
+void ticket_lock_acquire_bo(struct ticket_lock *lock, int backoff) {
+  int my_ticket = __atomic_fetch_add(&lock->next_ticket, 1, __ATOMIC_RELAXED);
+  for (;;) {
+    int ns = __atomic_load_n(&lock->now_serving, __ATOMIC_RELAXED);
+    if (ns == my_ticket)
+      break;
+    for (volatile int i = 0; i < (my_ticket - ns) * backoff; ++i)
+      ;  // spin
+  }
+  __atomic_thread_fence(__ATOMIC_ACQUIRE);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // SPIN_LOCK_TICKET_H
