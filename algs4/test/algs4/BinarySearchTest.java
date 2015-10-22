766f7740b7f0cebb544fe0771faff65d36b6ec64
Brian Brooks <brooks.brian@gmail.com>
2015-10-21 19:38:18 -0500

    More algs4/BinarySearchTest

--- algs4/test/algs4/BinarySearchTest.java
+++ algs4/test/algs4/BinarySearchTest.java
@@ -13,9 +13,27 @@ public class BinarySearchTest {
   }
 
   @Test
-  public void testFound1() {
+  public void testNotFound() {
+    int[] a = { 3 };
+    assertEquals(-1, BinarySearch.indexOf(a, 7));
+  }
+
+  @Test
+  public void testNotFound2() {
+    int[] a = { 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096 };
+    assertEquals(-1, BinarySearch.indexOf(a, 8192));
+  }
+
+  @Test
+  public void testFound() {
     int[] a = { 3 };
     assertEquals(0, BinarySearch.indexOf(a, 3));
+  }
+
+  @Test
+  public void testFound2() {
+    int[] a = { 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096 };
+    assertEquals(9, BinarySearch.indexOf(a, 1024));
   }
 
 }
